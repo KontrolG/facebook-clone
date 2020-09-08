b5ea64e40e27fe991c8f82c3d1cd138363afb0db
German Solano <kontrolg1239@gmail.com>
2020-09-08 11:25:30 -0400

    Arreglar Home para acoplarse a los nuevos Contexts

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
-import { Provider, getContextValue } from "../../../context";
-import useGetPosts from "../../../hooks/useGetPosts";
+import React from "react";
+import { PostsProvider } from "../../../contexts/PostsContext";
 import SearchBar from "../../searchBar/SearchBar";
 import PostsMain from "./PostsMain";
 import "./Home.css";
 
 const Home = () => {
-  const getPostsState = useGetPosts();
-  const searchQueryState = useState("");
-  const { user } = getContextValue();
-
   return (
-    <Provider value={{ user, getPostsState, searchQueryState }}>
+    <PostsProvider>
       <SearchBar />
       <PostsMain />
-    </Provider>
+    </PostsProvider>
   );
 };
 
