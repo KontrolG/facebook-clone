eb422918f64b8aa907dc5b2b0f0c69988598c65f
German Solano <kontrolg1239@gmail.com>
2020-09-09 10:31:04 -0400

    Añadir llaves adicionales de la configuracion de Firebase

--- src/firebase-utils/init.js
+++ src/firebase-utils/init.js
@@ -1,16 +1,22 @@
 import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/storage";
+import "firebase/auth";
 
-import { databaseURL, storageBucket } from "./keys";
+import { projectId, apiKey, appId } from "./keys";
 
 const config = {
-  databaseURL,
-  storageBucket
+  apiKey,
+  projectId,
+  appId,
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com/`,
+  storageBucket: `gs://${projectId}.appspot.com`
 };
 
 firebase.initializeApp(config);
 
+export default firebase;
 export const database = firebase.database();
 export const { TIMESTAMP } = firebase.database.ServerValue;
 export const storage = firebase.storage();
