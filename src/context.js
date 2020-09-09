255873e8a567720c558a5ac75b45a9ae686e16b8
German Solano <kontrolg1239@gmail.com>
2020-09-09 11:30:16 -0400

    Eliminar context anterior y renombrar useAuthState hook para mejor legibilidad de la intencion

--- src/context.js
+++
@@ -1,7 +0,0 @@
-import { useContext, createContext } from "react";
-
-const context = createContext();
-export default context;
-export const { Provider } = context;
-export const { Consumer } = context;
-export const getContextValue = () => useContext(context);
