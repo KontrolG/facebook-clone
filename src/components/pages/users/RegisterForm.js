694e523edc630eb698d54af60e93c33256eaa0ea
German Solano <kontrolg1239@gmail.com>
2020-09-10 06:00:29 -0400

    Añadir falso envio de formulario y cambiar estilo de las imagenes de un post

--- src/components/pages/users/RegisterForm.js
+++ src/components/pages/users/RegisterForm.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { v4 as getRandomId } from "uuid";
+import useForm from "../../../hooks/useForm";
+import { useUserContext } from "../../../contexts/UserContext";
 import Button from "../../Button";
 
 const RegisterForm = props => {
+  const [formState, changeFieldValue] = useForm("email", "password");
+
+  const { signup } = useUserContext();
+
   const sendUser = event => {
     event.preventDefault();
+    const fakeNewUser = {
+      email: `random${getRandomId()}@test.com`,
+      password: getRandomId(),
+      displayName: "Jhonanderson Doe",
+      photoURL:
+        "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/86181588_10158315845017189_3531420598666264576_o.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=OXWaMYPzk2gAX_JCtya&_nc_ht=scontent-mia3-1.xx&oh=c51631121785f7c9a5d8e284c7432675&oe=5F7DCBC2"
+    };
+    signup(fakeNewUser);
   };
 
   return (
@@ -17,11 +32,21 @@ const RegisterForm = props => {
       </div>
       <div className="wrapper">
         <label>Correo electronico</label>
-        <input type="email" placeholder="Introduce tu correo electronico" />
+        <input
+          type="email"
+          placeholder="Introduce tu correo electronico"
+          value={formState.email}
+          onChange={changeFieldValue}
+        />
       </div>
       <div className="wrapper">
         <label>Contraseña</label>
-        <input type="password" placeholder="Introduce tu contraseña" />
+        <input
+          type="password"
+          placeholder="Introduce tu contraseña"
+          value={formState.password}
+          onChange={changeFieldValue}
+        />
       </div>
       <div className="wrapper">
         <label>Confirmar contraseña</label>
