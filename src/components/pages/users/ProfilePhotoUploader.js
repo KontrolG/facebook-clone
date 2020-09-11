231b3c50116a42555e06fc77ea30cca12ba2981a
German Solano <kontrolg1239@gmail.com>
2020-09-11 06:45:49 -0400

    Cambiar input de UploadButton a incontrolador

--- src/components/pages/users/ProfilePhotoUploader.js
+++ src/components/pages/users/ProfilePhotoUploader.js
@@ -8,19 +8,15 @@ const imageFileTypes = "image/*,image/heif,image/heic";
 const defaultProfilePictureURL =
   "https://firebasestorage.googleapis.com/v0/b/fb-post-creator.appspot.com/o/profiles-pictures%2Fdefault-profile-picture.jpg?alt=media&token=f82f4d92-2d6e-4720-97d7-3e584dc527db";
 
-const renderUploadButtonChildren = (clickInput, inputRef) => {
+const renderUploadButtonChildren = (clickInput, restartInput) => {
   return (
     <>
       <Button onClick={clickInput} fullWidth primary>
         Cambiar
       </Button>
-      <Input
-        type="file"
-        accept={imageFileTypes}
-        name="profile-photo"
-        ref={inputRef}
-        hidden
-      />
+      <Button onClick={restartInput} fullWidth>
+        Eliminar
+      </Button>
     </>
   );
 };
@@ -36,7 +32,11 @@ const ProfilePhotoUploader = ({ onImageUpload, className }) => {
         defaultImageURL={"img/test.jpg" /* defaultProfilePictureURL */}
         rounded
       />
-      <UploadButton children={renderUploadButtonChildren} overlapping />
+      <UploadButton
+        children={renderUploadButtonChildren}
+        overlapping
+        imageFileTypes={imageFileTypes}
+      />
     </PhotoUploader>
   );
 };
--- src/components/photoUploader/UploadButton.js
+++ src/components/photoUploader/UploadButton.js
@@ -2,24 +2,40 @@ import React, { createRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { usePhotoUploaderContext } from "./context";
 
-const UploadButton = ({ children, overlapping }) => {
+const UploadButton = ({ children, imageFileTypes, overlapping }) => {
   const { setImage } = usePhotoUploaderContext();
   const inputRef = createRef();
-
-  const clickInput = () => {
-    inputRef.current.click();
-  };
-
-  useEffect(() => {
-    const [photo] = inputRef.current.files;
-    setImage(photo);
-  }, [inputRef]);
 
   const UploadButtonStyle = {
     marginTop: overlapping ? "-20%" : "0.5rem"
   };
 
-  return <div style={UploadButtonStyle}>{children(clickInput, inputRef)}</div>;
+  const clickInput = () => {
+    inputRef.current.click();
+  };
+
+  const restartInput = () => {
+    inputRef.current.value = null;
+    setImage(null);
+  };
+
+  const changeImage = ({ target }) => {
+    const [photo] = target.files;
+    setImage(photo);
+  };
+
+  return (
+    <div style={UploadButtonStyle}>
+      <input
+        type="file"
+        accept={imageFileTypes}
+        onChange={changeImage}
+        ref={inputRef}
+        hidden
+      />
+      {children(clickInput, restartInput)}
+    </div>
+  );
 };
 
 UploadButton.defaultProps = {
