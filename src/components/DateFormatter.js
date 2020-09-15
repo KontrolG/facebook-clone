415301b11b9f925f99800824d2c2f299d28ac6b3
German Solano <kontrolg1239@gmail.com>
2020-09-15 18:02:21 -0400

    Traducir a español unidades de tiempo en DateFormatter

--- src/components/DateFormatter.js
+++ src/components/DateFormatter.js
@@ -36,7 +36,7 @@ const getTimeInUnits = seconds => {
   const minutes = seconds / 60;
   const hours = minutes / 60;
   const days = hours / 24;
-  return { days, hours, minutes };
+  return { dias: days, horas: hours, minutos: minutes };
 };
 
 const getGreaterTimeUnit = timeInUnits => {
