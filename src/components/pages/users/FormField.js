4aaba92bb40bd0d0922d891e277dd86276dd4bfb
German Solano <kontrolg1239@gmail.com>
2020-09-17 11:07:02 -0400

    Añadir Popover a errores en los formularios

--- src/components/pages/users/FormField.js
+++ src/components/pages/users/FormField.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
-import { Input } from "../../form";
-import FormErrorMessage from "./FormErrorMessage";
+import { Input, ErrorMessage } from "../../form";
+import { Popover, ArrowContainer } from "react-tiny-popover";
 
-const FormField = ({ labelText, type, name, placeholder }) => (
-  <div className="wrapper">
-    <label htmlFor={name}>{labelText}</label>
-    <Input type={type} name={name} placeholder={placeholder} />
-    <FormErrorMessage fieldName={name} />
-  </div>
-);
+const popoverStyles = {
+  backgroundColor: "darkred",
+  color: "white",
+  borderRadius: "2px"
+};
+
+const FormField = ({ labelText, type, name, placeholder }) => {
+  const [error, setError] = useState(null);
+
+  const setErrorMessage = useCallback(errors => {
+    const ErrorMessage = errors?.[0]?.message;
+    setError(ErrorMessage);
+    return null;
+  }, []);
+
+  const renderArrowContainer = ({ childRect, popoverRect, position }) => (
+    <ArrowContainer
+      childRect={childRect}
+      popoverRect={popoverRect}
+      position={position}
+      arrowSize={8}
+      arrowColor={popoverStyles.backgroundColor}
+      className={`popover-arrow-${position}`}
+    >
+      {error}
+    </ArrowContainer>
+  );
+
+  return (
+    <div className="wrapper">
+      <label htmlFor={name}>{labelText}</label>
+      <Popover
+        isOpen={Boolean(error)}
+        positions={["right", "bottom"]}
+        containerStyle={popoverStyles}
+        padding={16}
+        containerClassName="form-error-message"
+        content={renderArrowContainer}
+      >
+        <Input type={type} name={name} placeholder={placeholder} />
+      </Popover>
+      <ErrorMessage fieldName={name}>{setErrorMessage}</ErrorMessage>
+    </div>
+  );
+};
 
 FormField.defaultProps = {
   type: "text",
